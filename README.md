6396898c278c7234d59eb4b842ecff7b5bbca2df
akilarlxh <akilarlxh@gmail.com>
2021-06-22 16:25:46 +0800

    0.0.2 test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hexo-butterfly-swiper
 
-给`hexo-theme-butterfly`添加 [侧边栏电子钟](https://akilar.top/posts/4e39cf4a/)
+给`hexo-theme-butterfly`添加 [首页轮播图](https://akilar.top/posts/8e1264d1/)
 
 # 安装
 
@@ -14,16 +14,16 @@
 
   ```yaml
     # hexo-butterfly-swiper
-    # see https://akilar.top/posts/4e39cf4a/
-    electric_swiper:
+    # see https://akilar.top/posts/8e1264d1/
+    swiper:
       enable: true # 开关
       priority: 5 #过滤器优先权
       enable_page: all # 应用页面
+      timemode: date #date/updated
       layout: # 挂载容器类型
-        type: class
-        name: sticky_layout
+        type: id
+        name: recent-posts
         index: 0
-      loading: #加载动画自定义
   ```
 3. 参数释义
 
@@ -32,9 +32,22 @@
   |priority|number|【可选】过滤器优先级，数值越小，执行越早，默认为10，选填|
   |enable|true/false|【必选】控制开关|
   |enable_page|path/all|【可选】填写想要应用的页面的相对路径（即路由地址）,如根目录就填'/',分类页面就填'/categories/'。若要应用于所有页面，就填'all'，默认为all|
+  |timemode|date/updated|【可选】时间显示，date为显示创建日期，updated为显示更新日期,默认为date|
   |layout.type|id/class|【可选】挂载容器类型，填写id或class，不填则默认为id|
   |layout.name|text|【必选】挂载容器名称|
   |layout.index|0和正整数|【可选】前提是layout.type为class，因为同一页面可能有多个class，此项用来确认究竟排在第几个顺位|
-  |loading|URL|【可选】电子钟加载动画的图片|
+
+4. 使用方法
+  在文章的`front_matter`中添加`swiper_index`配置项即可。
+  ```markdown
+  ---
+  title:
+  date:
+  updated:
+  cover:
+  description:
+  swiper_index:
+  ---
+  ```
 # 截图
-![](https://cdn.jsdelivr.net/npm/akilar-candyassets/image/Card-swiper-b0876bb5.png)
+![](https://cdn.jsdelivr.net/npm/akilar-candyassets/image/f4783623.png)
--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const pug = require('pug')
 const path = require('path')
 const urlFor = require('hexo-util').url_for.bind(hexo)
 const util = require('hexo-util')
+const stylus = require('stylus');
 // 过滤器优先级，priority 值越低，过滤器会越早执行，默认priority是10。
 const pre_priority = hexo.config.swiper.priority || hexo.theme.config.swiper.priority
 const priority = pre_priority ? pre_priority : 10
 
-hexo.extend.filter.register('after_generate', function (locals) {
+hexo.extend.filter.register('after_generate', function () {
+  // 获取所有文章
+  var posts_list = hexo.locals.get('posts').data;
+  var swiper_list = []
+  // 若文章的front_matter内设置了index和描述，则将其放到swiper_list内
+  for (item of posts_list) {
+    if (item.swiper_index && item.swiper_desc) {
+      swiper_list.push(item)
+    }
+  }
+  // 对swiper_list进行处理，使其按照index大小进行排序
+  function sortNumber(a, b) {
+    return a.swiper_index - b.swiper_index
+  }
+  swiper_list = swiper_list.sort(sortNumber);
+  swiper_list = swiper_list.reverse();
+// =====================================================================
   // 首先获取整体的配置项名称
   const config = hexo.config.swiper || hexo.theme.config.swiper
   // 如果配置开启
@@ -18,18 +35,26 @@ hexo.extend.filter.register('after_generate', function (locals) {
   // 集体声明配置项
     const data = {
       enable_page: config.enable_page ? config.enable_page : "all",
+      timemode: config.timemode ? config.timemode : "date",
       layout_type: config.layout.type,
       layout_name: config.layout.name,
       layout_index: config.layout.index ? config.layout.index : 0,
-      plus_style: config.plus_style ? urlFor(config.plus_style) : ""
+      error_img: hexo.theme.config.error_img.post_page,
+      swiper_list: swiper_list
     }
   // 渲染页面
-  const temple_html_text = config.temple_html ? config.temple_html : pug.renderFile(path.join(__dirname, './lib/html.pug'),data)
+  const temple_html_text = config.temple_html ? config.temple_html : pug.renderFile(path.join(__dirname, './lib/html.pug'),data);
+
+  var str = require('fs').readFileSync(__dirname + './lib/swiperstyle.styl', 'utf8');
+  hexo.extend.renderer.register('styl', 'css', function(str,callback){
+  stylus(data.text).set('swiperstyle', './lib/swiperstyle.styl').render(callback);
+  });
   //cdn资源声明
     //样式资源
-  const css_text = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hexo-butterfly-swiper/lib/swiper.min.css">`
+  const css_text = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/css/swiper.min.css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/hexo-butterfly-swiper/lib/swiperstyle.css">`
     //脚本资源
-  const js_text = `<script src="https://pv.sohu.com/cityjson?ie=utf-8"></script><script data-pjax src="https://cdn.jsdelivr.net/npm/hexo-butterfly-swiper/lib/swiper.min.js"></script>`
+  const js_text = `<script defer src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.1.6/js/swiper.min.js"></script><script defer data-pjax src="https://cdn.jsdelivr.net/npm/hexo-butterfly-swiper/lib/swiper_init.js"></script>`
+
   //注入容器声明
   var get_layout
   //若指定为class类型的容器
@@ -68,70 +93,3 @@ hexo.extend.filter.register('after_generate', function (locals) {
   // 注入脚本资源
   hexo.extend.injector.register('head_end', css_text, "default");
 },priority)
-
-
-
-// 
-// function priority_swiper(){
-//     var priority = 0
-//     if(hexo.config.swiper.priority){
-//         priority = hexo.config.swiper.priority
-//     }
-//     else{
-//         priority = 0
-//     }
-//     return priority
-// }
-
-hexo.extend.filter.register('after_generate',function() {
-    var swiper = hexo.config.swiper.enable;
-    if(swiper){
-        var temple = hexo.config.swiper.temple_html.split("${temple_html_item}")
-        var layout_name ='';
-        var layout_type ='';
-        var layout_index =0;
-        layout_name = hexo.config.swiper.layout.name;
-        layout_type = hexo.config.swiper.layout.type;
-        layout_index =  hexo.config.swiper.layout.index;
-        var get_layout = ''
-        if (layout_type == 'class'){
-            get_layout =  `document.getElementsByClassName('${layout_name}')[${layout_index}]`
-        }else if (layout_type == 'id'){
-            get_layout =  `document.getElementById('${layout_name}')`
-        }else {
-            get_layout =  `document.getElementById('${layout_name}')`
-        }
-        var temple_html_item = ''
-        var posts_list = hexo.locals.get('posts').data;
-        var new_posts_list =[]
-        for (item of posts_list){
-            if(item.swiper_index && item.swiper_desc){
-                new_posts_list.push(item)
-            }}
-        function sortNumber(a,b)
-        {
-            return a.swiper_index - b.swiper_index
-        }
-        new_posts_list = new_posts_list.sort(sortNumber);
-        new_posts_list = new_posts_list.reverse();
-        for (item of new_posts_list){
-            if(item.swiper_index && item.swiper_desc){
-                temple_html_item += `<div class="blog-slider__item swiper-slide" style="width: 750px; opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms"><div class="blog-slider__img"><img src="${item.swiper_cover||item.cover}" alt="${item.swiper_cover||item.cover}"/></div><div class="blog-slider__content"><span class="blog-slider__code">${item.date.format('YYYY-MM-DD')}</span><a class="blog-slider__title" href="${item.path}">${item.title}</a><div class="blog-slider__text">${item.swiper_desc}</div><a class="blog-slider__button" href="${item.path}">详情</a></div></div>`;
-            }
-        }
-        var temple_html =`${temple[0]}<div class="blog-slider__wrp swiper-wrapper" style="transition-duration: 0ms">${temple_html_item}</div><div class="blog-slider__pagination swiper-pagination-clickable swiper-pagination-bullets"></div>${temple[1]}`;
-        var script_text = ` <script data-pjax>if(${get_layout} && location.pathname =='${hexo.config.swiper.enable_page}'){
-
-    var parent = ${get_layout};
-    var child = '${temple_html}';
-    console.log('已挂载swiper')
-    parent.insertAdjacentHTML("afterbegin",child)}
-     </script>
-<script data-pjax src="https://cdn.jsdelivr.net/gh/Zfour/Butterfly-swiper/swiper/swiper.min.js"></script>
-<script data-pjax src="https://cdn.jsdelivr.net/gh/Zfour/Butterfly-swiper@0.18/swiper/swiperindex.js"></script>
-<style>${hexo.config.swiper.plus_style}</style>`;
-        hexo.extend.injector.register('head_end',`<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zfour/Butterfly-swiper/swiper/swiper.min.css"><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Zfour/Butterfly-swiper/swiper/swiperstyle.css">`, "default");
-        hexo.extend.injector.register('body_end',script_text, "default");
-    }
-
-},priority_swiper())
