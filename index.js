4ba4f37cc72eabcf06f6d3384c91c72a6c1c7c07
akilarlxh <akilarlxh@gmail.com>
2021-06-24 15:36:22 +0800

    1.0.3 修复读取不到主题配置项的bug

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const pug = require('pug')
 const path = require('path')
 const urlFor = require('hexo-util').url_for.bind(hexo)
 const util = require('hexo-util')
-// 过滤器优先级，priority 值越低，过滤器会越早执行，默认priority是10。
-const pre_priority = hexo.config.swiper.priority ? hexo.config.swiper.priority : hexo.theme.config.swiper.priority
-const priority = pre_priority ? pre_priority : 10
 
 hexo.extend.filter.register('after_generate', function () {
   // 获取所有文章
@@ -89,4 +86,11 @@ hexo.extend.filter.register('after_generate', function () {
   hexo.extend.injector.register('body_end', js_text, "default");
   // 注入脚本资源
   hexo.extend.injector.register('head_end', css_text, "default");
-},priority)
+},
+hexo.extend.helper.register('priority', function(){
+  // 过滤器优先级，priority 值越低，过滤器会越早执行，默认priority是10
+  const pre_priority = hexo.config.electric_clock.priority ?  hexo.config.electric_clock.priority : hexo.theme.config.electric_clock.priority
+  const priority = pre_priority ? pre_priority : 10
+  return priority
+})
+)
